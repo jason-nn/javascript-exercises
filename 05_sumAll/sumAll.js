3a9e9249ef5d92ee761ef85c3b992b6e9b838553
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-31 17:20:32 +0800

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  if (
+    typeof start === 'number' &&
+    typeof end === 'number' &&
+    start >= 0 &&
+    end >= 0
+  ) {
+    nums = Math.abs(end - start) + 1;
+    return ((start + end) * nums) / 2;
+  } else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
