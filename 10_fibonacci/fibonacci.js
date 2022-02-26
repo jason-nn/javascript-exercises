4f57e819e4634168afa079b9237628fa92e0b2c6
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-26 16:06:35 +0800

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  if (a < 1) return 'OOPS';
+  if (a == 1 || a == 2) {
+    return 1;
+  } else {
+    return fibonacci(a - 1) + fibonacci(a - 2);
+  }
 };
 
 // Do not edit below this line
