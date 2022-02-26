cae78874ecc57167fced10d6cacd98ef6a1955a9
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-26 16:19:55 +0800

    Finish get the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.sort(
+    (a, b) =>
+      (b.yearOfDeath ? b.yearOfDeath : new Date().getFullYear()) -
+      b.yearOfBirth -
+      (a.yearOfDeath ? a.yearOfDeath : new Date().getFullYear()) +
+      a.yearOfBirth
+  )[0];
 };
+
+const people = [
+  {
+    name: 'Carly',
+    yearOfBirth: 0,
+    yearOfDeath: 1,
+  },
+  {
+    name: 'Ray',
+    yearOfBirth: 0,
+    yearOfDeath: 10,
+  },
+  {
+    name: 'Jane',
+    yearOfBirth: 0,
+    yearOfDeath: 5,
+  },
+];
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
