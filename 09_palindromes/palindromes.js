6c9359def7dddc0501f6196455f46c5b267d0d22
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-26 15:58:13 +0800

    Finish palindrome activity

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+  cleaned = a.replace(/[^a-zA-Z]+/g, '').toLowerCase();
+  for (let i = 0; i <= cleaned.length / 2; i++) {
+    if (!(cleaned[i] === cleaned[cleaned.length - i - 1])) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
